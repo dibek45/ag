0d8b33041b49efc0d2c61d6e0ed484f7f8694435
Dibek <dibektube@gmail.com>
2025-08-18 23:28:14 -0600

    agenda

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import { registerLocaleData } from '@angular/common';
+import localeEsMx from '@angular/common/locales/es-MX';
 
 // 🔥 Limpieza automática solo en desarrollo
 
+registerLocaleData(localeEsMx, 'es-MX');
 
 // 🚀 Bootstrap normal
 bootstrapApplication(AppComponent, appConfig)
